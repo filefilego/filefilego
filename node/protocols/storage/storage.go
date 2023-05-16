bbafbfc08799b2592f03c6783935f13c9162286c
filefilegoadmin <younixccp+ffg@gmail.com>
2023-05-16 21:54:31 +0300

    Add return in case of error

--- node/protocols/storage/storage.go
+++ node/protocols/storage/storage.go
@@ -334,6 +334,7 @@ func (p *Protocol) handleIncomingStorageQueryResponse(s network.Stream) {
 	ok, err := publicKeyStorageProvider.Verify(hash, tmp.Signature)
 	if err != nil || !ok {
 		log.Error("failed to verify storage query response")
+		return
 	}
 
 	p.mu.Lock()
@@ -345,6 +346,7 @@ func (p *Protocol) handleIncomingStorageQueryResponse(s network.Stream) {
 func (p *Protocol) GetDiscoveredStorageProviders() []*messages.StorageQueryResponseProto {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
+
 	providers := make([]*messages.StorageQueryResponseProto, 0)
 	for _, v := range p.storageProviders {
 		providers = append(providers, v)
