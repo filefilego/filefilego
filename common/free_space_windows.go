e8b9fe5094c1a2688b964a2cbfe24d4863b425ed
filefilegoadmin <younixccp+ffg@gmail.com>
2023-05-21 15:09:02 +0300

    Fix the windows version

--- common/free_space_windows.go
+++ common/free_space_windows.go
@@ -4,20 +4,17 @@ import "golang.org/x/sys/windows"
 
 // GetDirectoryFreeSpace returns the available space of a directory.
 func GetDirectoryFreeSpace(directoryPath string) (uint64, error) {
-	var freeSpace uint64
-	var err error
-
 	var free, total, avail uint64
 
 	pathPtr, err := windows.UTF16PtrFromString(directoryPath)
 	if err != nil {
-		panic(err)
+		return err
 	}
+
 	err = windows.GetDiskFreeSpaceEx(pathPtr, &free, &total, &avail)
-
-	if err == nil {
-		freeSpace = free
+	if err != nil {
+		return err
 	}
 
-	return freeSpace, err
+	return free, nil
 }
